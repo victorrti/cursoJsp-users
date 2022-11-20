d4d5b5a014697f07f19a04bd18feaec36949b70b
Victor Pereira <victorrodriguesti@gmail.com>
2022-11-20 09:45:47 -0300

    RECEBENDO DADOS DO CADASTRADO DE USUARIO E REDIRECIONANDO PARA A TELA DE USUARIO

---
+++ curso-jsp/src/main/java/servlets/ServletUsuarioController.java
@@ -0,0 +1,48 @@
+package servlets;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.ModelLogin;
+
+import java.io.IOException;
+public class ServletUsuarioController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public ServletUsuarioController() {
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	public void post(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 doPost(request,response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+				String id = request.getParameter("id");
+				String nome = request.getParameter("nome");
+				String login = request.getParameter("login");
+				String email = request.getParameter("email");
+				String  password = request.getParameter("password");
+				ModelLogin modelLogin = new ModelLogin();
+				
+				modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id): null);
+				modelLogin.setLogin(email);
+				modelLogin.setNome(nome);
+				modelLogin.setPassword(password);
+				modelLogin.setLogin(login);
+				
+				RequestDispatcher redireciona = request.getRequestDispatcher("principal/usuario.jsp");
+				redireciona.forward(request, response);
+	}
+
+}
